842adeebc8dc2f485973d2a3671c5663bcdf90ca
Orion Henry <orion@heroku.com>
2011-10-10 16:01:30 -0700

    add js to select boxes

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
 
 // add js for game here
 
+$(function() {
+  var moves = ["circle","cross"]
+  $(".cell").click(function(e) {
+    $(e.target).addClass(moves[0])
+    moves.reverse();
+  });
+});
+
